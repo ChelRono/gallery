9637e6c2e3d589a9403535a67cb9846319fc1568
Valarie <valarierono97@gmail.com>
2024-03-21 17:00:07 +0300

    Fetchs data from the database

--- photoapp/views.py
+++ photoapp/views.py
@@ -4,27 +4,6 @@ from .models import Gallery
 # Create your views here.
 def index(request):
 
-    gall1 = Gallery()
-    gall1.name = 'Hello there'
-    gall1.desc = 'This is hello world'
-    gall1.img = 'flower1.jpeg'
-
-    gall2 = Gallery()
-    gall2.name = 'Hello world'
-    gall2.desc = 'This is hello world'
-    gall2.img = 'mountain.jpeg'
-
-    gall3 = Gallery()
-    gall3.name = 'Hello Kenya'
-    gall3.desc = 'This is hello world'
-    gall3.img = 'flower1.jpeg'
-
-    gall4 = Gallery()
-    gall4.name = 'Hello Kenya'
-    gall4.desc = 'This is hello world'
-    gall4.img = 'flower1.jpeg'
-
-    galls = [gall1,gall2,gall3,gall4]
-
+    galls = Gallery.objects.all()
 
     return render(request,'sidebar.html',{'galls' : galls})
