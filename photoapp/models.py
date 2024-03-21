87bf183ce02d86fe2ac46af7f45f7571a3a932ed
Valarie <valarierono97@gmail.com>
2024-03-21 16:13:24 +0300

    Adds images to the gallery

--- photoapp/models.py
+++ photoapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Gallery:
+    name : str
+    desc : str
+    img : str
--- photoapp/views.py
+++ photoapp/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from .models import Gallery
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+
+    gall1 = Gallery()
+    gall1.name = 'Hello there'
+    gall1.desc = 'This is hello world'
+    gall1.img = 'flower1.jpeg'
+
+    gall2 = Gallery()
+    gall2.name = 'Hello world'
+    gall2.desc = 'This is hello world'
+    gall2.img = 'mountain.jpeg'
+
+    gall3 = Gallery()
+    gall3.name = 'Hello Kenya'
+    gall3.desc = 'This is hello world'
+    gall3.img = 'flower1.jpeg'
+
+    gall4 = Gallery()
+    gall4.name = 'Hello Kenya'
+    gall4.desc = 'This is hello world'
+    gall4.img = 'flower1.jpeg'
+
+    galls = [gall1,gall2,gall3,gall4]
+
+
+    return render(request,'sidebar.html',{'galls' : galls})
